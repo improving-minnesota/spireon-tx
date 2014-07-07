d10b11e8fd1b1e6ea1b40044fc705872aac7edb3
sabersd <sabersd@gmail.com>
2014-07-07 10:42:20 -0500

    adding the name custom validator

--- questionApp/grails-app/domain/com/opi/Person.groovy
+++ questionApp/grails-app/domain/com/opi/Person.groovy
@@ -8,8 +8,18 @@ class Person {
 
 	static constraints = {
 		username nullable: false, blank: false, maxSize: 30
-		firstName nullable: false, blank: false, maxSize: 30
+		firstName nullable: false, blank: false, maxSize: 30,
+			validator: { val, obj ->
+				if ((val?.toLowerCase() == 'justin')
+					&& (obj.lastName?.toLowerCase() == 'bieber')) {
+					return 'no.biebers.allowed'
+				}
+			}
 		lastName nullable: false, blank: false, maxSize: 30
 		email nullable: false, blank: false, email: true
+	}
+
+	String toString() {
+		return "$firstName $lastName"
 	}
 }
