593fae9f102a2756af69ae2156278cbf25664a43
sabersd <sabersd@gmail.com>
2014-07-14 16:19:38 -0500

    adding simple integration test

---
+++ questionApp/test/integration/com/opi/QuestionControllerSpec.groovy
@@ -0,0 +1,32 @@
+package com.opi
+
+
+
+import spock.lang.*
+
+/**
+ *
+ */
+class QuestionControllerSpec extends Specification {
+	QuestionController controller
+
+    def setup() {
+		controller = new QuestionController()
+	}
+
+    def cleanup() {
+    }
+
+	void "Question Controller returns json"() {
+		given:
+		controller.response.format = 'json'
+		controller.params.id = '1'
+
+		when:
+		controller.show()x
+
+		then:
+		controller.response.status == 200
+		controller.response.contentType == 'application/json;charset=UTF-8'
+	}
+}
