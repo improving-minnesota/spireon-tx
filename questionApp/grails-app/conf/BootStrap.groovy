a6dcfb52c082e91a2a69f8b444032bdf4de0c98a
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-07-21 00:02:31 -0500

    Ensuring base roles are created

--- questionApp/grails-app/conf/BootStrap.groovy
+++ questionApp/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
 class BootStrap {
 
     def init = { servletContext ->
+        // Ensure the ROLE_USER and ROLE_ADMIN authorities exists
+        com.opi.Role.findOrSaveByAuthority('ROLE_USER')
+        com.opi.Role.findOrSaveByAuthority('ROLE_ADMIN')
     }
     def destroy = {
     }
