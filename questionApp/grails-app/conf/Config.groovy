567cec5c70056e358542e0352132f2e4969417d6
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-07-20 19:14:03 -0500

    s2-quickstart (resolve User domain)

--- questionApp/grails-app/conf/Config.groovy
+++ questionApp/grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.opi.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.opi.UserRole'
+grails.plugin.springsecurity.authority.className = 'com.opi.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/assets/**':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
--- questionApp/grails-app/domain/com/opi/User.groovy
+++ questionApp/grails-app/domain/com/opi/User.groovy
@@ -1,15 +1,29 @@
 package com.opi
 
 class User {
+
+	transient springSecurityService
+
 	String username
+
 	String firstName
 	String lastName
 	String email
+
+	String password
+	boolean enabled = true
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
 	Date dateCreated
 	Date lastUpdated
 
+	static transients = ['springSecurityService']
+
 	static constraints = {
-		username nullable: false, blank: false, maxSize: 30
+		username nullable: false, blank: false, unique: true, maxSize: 30
+		password blank: false
 		firstName nullable: false, blank: false, maxSize: 30,
 			validator: { val, obj ->
 				if ((val?.toLowerCase() == 'justin')
@@ -19,9 +33,32 @@ class User {
 			}
 		lastName nullable: false, blank: false, maxSize: 30
 		email nullable: false, blank: false, email: true
+		}
+
+
+	static mapping = {
+		password column: '`password`'
+	}
+
+	Set<Role> getAuthorities() {
+		UserRole.findAllByUser(this).collect { it.role }
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
 	}
 
 	String toString() {
 		return "$firstName $lastName"
+	}
+
+	protected void encodePassword() {
+		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
 	}
 }
