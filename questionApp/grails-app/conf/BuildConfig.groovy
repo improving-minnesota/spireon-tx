a7d1a7bc732a2297765ea08832ddb4dd109cf3cd
jamesranstrom <james.ranstrom@objectpartners.com>
2014-07-23 00:13:26 -0700

    finish workshop

--- questionApp/grails-app/conf/BuildConfig.groovy
+++ questionApp/grails-app/conf/BuildConfig.groovy
@@ -21,6 +21,8 @@ grails.project.fork = [
 	console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
+//grails.plugin.location.'event-log'='../event-log'
+
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
 	// inherit Grails' default dependencies
--- questionApp/grails-app/services/com/opi/VotingService.groovy
+++ questionApp/grails-app/services/com/opi/VotingService.groovy
@@ -5,21 +5,29 @@ import grails.transaction.Transactional
 @Transactional
 class VotingService {
 
+    def eventService
+
 	Vote addVoteToQuestion(Question question, User votingUser, Integer vote) {
 		if (!question) {
 			throw new IllegalArgumentException()
 		}
 		def userVote = question.votes.find { it.user == votingUser }
-		userVote = userVote ?: new Vote(
-			user: votingUser
-		)
+		userVote = userVote ?: new Vote(user: votingUser)
 		userVote.vote = vote
 		userVote.save(failOnError: true)
 
 		question.addToVotes(userVote)
 		question.save(failOnError: true)
 
-		return userVote
+        def eventDetails = [:]
+        eventDetails << ['eventType':'addVoteToQuestion']
+        eventDetails << ['questionId': "${question.id}"]
+        eventDetails << ['votingUserId': "${votingUser.id}"]
+        eventDetails << ['vote': "$vote"]
+
+        eventService.createEvent(new Date(), 'questionApp', eventDetails)
+
+        return userVote
 	}
 
 	Vote addVoteToAnswer(Answer answer, User votingUser, Integer vote) {
@@ -27,15 +35,20 @@ class VotingService {
 			throw new IllegalArgumentException()
 		}
 		def userVote = answer.votes.find { it.user == votingUser }
-		userVote = userVote ?: new Vote(
-			user: votingUser
-		)
+		userVote = userVote ?: new Vote(user: votingUser)
 		userVote.vote = vote
 		userVote.save(failOnError: true)
 
 		answer.addToVotes(userVote)
 		answer.save(failOnError: true)
 
+        def eventDetails = [:]
+        eventDetails << ['eventType':'addVoteToAnswer']
+        eventDetails << ['answerId': "${answer.id}"]
+        eventDetails << ['votingUserId': "${votingUser.id}"]
+        eventDetails << ['vote': "$vote"]
+
+        eventService.createEvent(new Date(), 'questionApp', eventDetails)
 		return userVote
 	}
 
