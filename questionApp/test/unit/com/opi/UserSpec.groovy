985fbe0f1d8a83bba61f527fbd58c449f940469d
sabersd <sabersd@gmail.com>
2014-09-18 10:25:19 -0500

    moving the user tests to the UserSpec

--- questionApp/test/unit/com/opi/UserSpec.groovy
+++ questionApp/test/unit/com/opi/UserSpec.groovy
@@ -8,7 +8,6 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @Build(Question)
-
 @TestFor(User)
 class UserSpec extends Specification {
 
